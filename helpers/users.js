5bd1ad1eb716bd37453deba551d4a52aac83b081
Brandon Lugo <neobrandll@gmail.com>
2019-04-12 04:42:08 -0400

    fixing responses

--- helpers/users.js
+++ helpers/users.js
@@ -72,7 +72,7 @@ module.exports.updateProfile = async (req, username, email, name, phone) => {
 module.exports.updateProfilePicture = async (req) => {
     try{
     const user = req.user;
-    const image = `./public/uploads/${user.users_id}/${req.file.filename}`;
+    const image = `../public/uploads/${user.users_id}/${req.file.filename}`;
     const value = req.file.path;
     user.user_picture_url = value;
     await db.none(sql.updatePicture, [value, user.users_id]);
