a29d9f8b339bb4a37f46a7d08229b58aa1ee3800
Luis A. Acurero <13luismb@gmail.com>
2019-04-15 15:57:38 -0400

    initial socketio implementation

--- controllers/conversation.js
+++ controllers/conversation.js
@@ -1,23 +1,17 @@
 const express = require('express');
-const passport = require('passport');
 const auth = require('./../middlewares/jwtAuth');
-const jwt = require('jsonwebtoken');
 let router = express.Router();
-const config = require('./../helpers/config');
-const server = require('./../index.js');
 const Chat = require('./../helpers/conversation');
 
 
-
     router.post('/newChat', async (req,res)=>{
-        const io = req.app.get('socketio');
+       // const io = req.app.get('socketio');
         const resp = await Chat.newConversation(req, req.body.type, req.body.converName, req.body.users);
         if (resp.status === 200){
             socket.join(resp.conversation_id);
             res.status(200).send(resp)
         }
     });
-
 
     router.get('/chats', auth, async (req,res)=>{
         try{
--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const jwt = require('express-jwt');
 const config = require('./helpers/config');
 let passport = require('passport');
 let strategies = require('./helpers/strategies');
+const Conversation = require('./helpers/conversation');
+const Message = require('./helpers/message');
 //const fileUpload = require('express-fileupload');
 let auth = require('./middlewares/isAuth');
-
+let http = require("http").Server(app);
+let io = require("socket.io")(http);
 
 app.use('/views', express.static(__dirname + '/public'));
 app.use(express.json());
@@ -24,7 +27,7 @@ app.use('/', require('./controllers'));
 app.get('/', function(req, res) {
     res.redirect('views/index.html');
 });
-const models_path = __dirname+'/helpers';
+//const models_path = __dirname+'/helpers';
 // fs.readdirSync(models_path).forEach(file => {
 //   require(models_path+'/'+file);
 // });
@@ -38,6 +41,30 @@ passport.serializeUser(function(user, done) {
 passport.deserializeUser(function(user, done) {
     done(null, user);
 });
-app.listen(config.port, function() {
+http.listen(config.port, function() {
     console.log('Example app listening on port 3001!');
+});
+
+
+io.sockets.on('connection', socket => {
+        socket.on('send-message', message => {
+            
+        });
+
+        socket.on('enter-chat', async data => {
+            const resp = await Message.getListMessage(data.chatId);
+            socket.emit('get-messages', resp);
+        });
+
+        socket.on('initiate-chat', async data => {
+            const resp = await Conversation.newConversation(data.token, data.type, data.converName, data.users);
+            socket.emit('enter-chat', resp);
+        });
+
+        socket.on("disconnect", function() {
+            console.log('bye')
+            let index = users[socket.room].findIndex(el => el === socket.nickname);
+            users[socket.room].splice(index, 1);
+          });
+
 });
